1d87e26cc42ca0de67644d3732d7e55b4c299269
John Renner <johnmrenner@gmail.com>
2012-01-19 21:13:39 -0500

    Re-refactored db module

--- modules/db.js
+++ modules/db.js
@@ -2,9 +2,11 @@
  * Manage database schemas and connections
  * @module db
  */
+var mongoose = require('mongoose');
 
 this.connect = function(){
-  var con = require('mongoose');
-  con.connect('localhost','intranet',1337);
-  return con;
+  var db = mongoose.createConnection('localhost','intranet',1337);
+  return db;
 };
+
+this.schemas = {};
